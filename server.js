5b5393d34e493c00c9c4ea6f31a3506eced23bf3
unknown <hero1998f@gmail.com>
2022-08-12 19:14:54 +0300

    ss commj5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 1998;
+const port = process.env.PORT || 3500;
 
 server.use(middlewares);
 server.use(router);
