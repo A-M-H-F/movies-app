126027f85741188686be9818457ff1afa0da5dec
unknown <hero1998f@gmail.com>
2022-08-12 21:35:20 +0300

    fsets

--- src/components/FilterSearch.jsx
+++ src/components/FilterSearch.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable */
 import { Link } from 'react-router-dom';
 function FilterSearchData({ filteredResults }) {
-    const urlRegex = /\s/g;
     return (
         filteredResults.map((movie) => (
             <div key={movie.id} className="movie">
-                <Link to={`/${movie.id}/${movie.title.toLowerCase().replace(urlRegex, '-')}`} style={{ textDecoration: 'none' }}>
+                <Link to={`/movies/${movie.id}}`} style={{ textDecoration: 'none' }}>
                     <img src={movie.images['Poster Art'].url} alt="" />
 
                     <div className="movie-info">
--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -2,12 +2,11 @@
 import { Link } from 'react-router-dom';
 // Get All Movies Data and Build a movie card for each one / add a unique link for each movie to redirect view movie details page
 function MovieCard({ filteredType, moreMovies }) {
-    const urlRegex = /\s/g;
     return (
       filteredType.slice(0, moreMovies).map((movie) => (
 
             <div key={movie.id} className="movie">
-              <Link to={`/${movie.id}/${movie.title.toLowerCase().replace(urlRegex, '-')}`} style={{ textDecoration: 'none' }}>
+              <Link to={`/movies/${movie.id}}`} style={{ textDecoration: 'none' }}>
                 <img src={movie.images['Poster Art'].url} alt="cover" />
                 <div className="movie-info">
                   <span>{movie.releaseYear}</span>
