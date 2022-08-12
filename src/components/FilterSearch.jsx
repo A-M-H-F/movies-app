908ed1cc182ec95f1d97cb53df60885c8121ffcc
unknown <hero1998f@gmail.com>
2022-08-12 21:28:05 +0300

    fssss

--- src/components/FilterSearch.jsx
+++ src/components/FilterSearch.jsx
@@ -5,7 +5,7 @@ function FilterSearchData({ filteredResults }) {
     return (
         filteredResults.map((movie) => (
             <div key={movie.id} className="movie">
-                <Link to={`/movies/${movie.id}/${movie.title.toLowerCase().replace(urlRegex, '-')}`} style={{ textDecoration: 'none' }}>
+                <Link to={`/${movie.id}/${movie.title.toLowerCase().replace(urlRegex, '-')}`} style={{ textDecoration: 'none' }}>
                     <img src={movie.images['Poster Art'].url} alt="" />
 
                     <div className="movie-info">
