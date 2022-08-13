ba1f0319450b0de7ddcc3e5c8f2fd7841a77e4fa
unknown <hero1998f@gmail.com>
2022-08-13 21:42:34 +0300

    amggggkkk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <footer className="text-center text-white footer-cus mt-auto">
           <div className="text-center p-3">
             © 2022 Copyright:
-            <a className="text-white" href="https://grow.phoenix-lb.net" target="_blank" rel="noreferrer"> Hussein Istanbouli</a>
+            <a className="text-white" href="https://grow.phoenix-lb.net" target="_blank" rel="noreferrer"> Hussein (Farhat) Istanbouli</a>
           </div>
         </footer>
       </div>
