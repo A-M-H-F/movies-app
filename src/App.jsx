863ff89a24389cf24ea95bdeb50aabd6247d4462
unknown <hero1998f@gmail.com>
2022-08-14 04:28:16 +0300

    amhfMainDone28

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router basename="/home">
+    <Router basename="/#">
       <div className="App">
         <Navbar />
         <div className="content">
